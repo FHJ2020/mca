e326571d6287107379f17b12dc61c643065356e1
FHJ <429785208@qq.com>
2022-09-11 17:39:08 +0800

    change dir

--- src/main/java/mca/com/sean/io/test/service/Car.java
+++
@@ -1,5 +0,0 @@
-package mca.com.sean.io.test.service;
-
-public interface Car{
-    public String ooxx(String msg);
-}
--- src/main/java/mca/com/sean/io/test/service/Fly.java
+++
@@ -1,5 +0,0 @@
-package mca.com.sean.io.test.service;
-
-public interface Fly{
-    void xxoo(String msg);
-}
